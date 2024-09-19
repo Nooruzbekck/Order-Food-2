81e673d1a9a9dcadf062baf429bb48e6aace00b7
Nooruz <abdykalykovnooruz638@gmail.com>
2024-09-19 23:27:32 +0300

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
-import React from "react";
-import InfoProduct from "./components/InfoProductCard/InfoProduct";
-import Banner from "./components/BannerMain/Banner";
-// import Banner from "./components/BannerMain/Banner.png";
-
 const App = () => {
-  return (
-    <div>
-      <Banner />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
