fa8737ebf41b2a1112e4e8448a20f09492e70def
Aitula-svg <aitu.ru00@gmail.com>
2024-09-19 18:19:48 +0300

    finish feature/main-banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
-function App() {
-  return <div></div>;
-}
+import React from "react";
+import InfoProduct from "./components/InfoProductCard/InfoProduct";
+import Banner from "./components/BannerMain/Banner";
+// import Banner from "./components/BannerMain/Banner.png";
+
+const App = () => {
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
+};
 
 export default App;
