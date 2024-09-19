1f53b512f9d08357dff036a817ac2445489c7f88
Aitula-svg <aitu.ru00@gmail.com>
2024-09-19 21:51:46 +0300

    feature/main

--- src/components/BannerMain/Banner.jsx
+++ src/components/BannerMain/Banner.jsx
@@ -1,11 +1,14 @@
 import Bannerr from "../../assets/images/banner.png";
 import InfoProduct from "../InfoProductCard/InfoProduct";
+import "./Banner.css";
 
 const Banner = () => {
   return (
     <div className="image-banner">
-      <img src={Bannerr} alt="banner" />
-      <InfoProduct />
+      <img src={Bannerr} alt="banner" className="banner" />
+      <div className="wrapper-info-product">
+        <InfoProduct />
+      </div>
     </div>
   );
 };
